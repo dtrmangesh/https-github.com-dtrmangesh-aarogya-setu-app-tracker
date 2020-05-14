b334e363e54c53f3982b31615f19a8a30c0a958e
Anjali Kapare <akapare@mobiquityinc.com>
2020-05-14 14:36:12 +0530

    AST-5: Implemented home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ interface DeviceHardware {
   isBletoothTurnOn: boolean,
   isGPSOn:boolean
 }
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -17,11 +18,25 @@ export class HomePage {
   };
   constructor(private diagnostic: DiagnosticService) {
   }
+
+  cardTitles = ['Confirmed cases', 'Active cases', 'Recovered cases', 'Deceased cases'];
+  sliderImages= ['assets/images/cover_mouth_sneeze.png',
+  'assets/images/wash_hands_2.png',
+  'assets/images/social_distance.png',
+  'assets/images/stay_home.png']
+  userName= 'Anjali';
+
+  sliderOpts = {
+    autoplay: true,
+    zoom: {
+      maxRatio: 5
+    }
+  };
+
   async ionViewWillEnter() {
     this.hardwareAvailiability.isBletoothTurnOn =  await  this.diagnostic.checkBluetoothAvailability();
-    console.log("bluetooth",this.hardwareAvailiability.isBletoothTurnOn);
+    console.log('bluetooth',this.hardwareAvailiability.isBletoothTurnOn);
     this.hardwareAvailiability.isGPSOn = await this.diagnostic.checkGPSAvailability();
-    console.log("GPS",this.hardwareAvailiability.isGPSOn);
+    console.log('GPS',this.hardwareAvailiability.isGPSOn);
   }
-
 }
